73fc647ab92116eb7b74e4fe5bce668cfde27f68
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-16 10:03:29 +0300

    task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0; 
+
+if (a > b) {
+    max = a; 
+    Console.WriteLine("max = " + max);
+} else if (a < b) {
+    max = b; 
+    Console.WriteLine("max = " + max);
+} else {
+    Console.WriteLine("Numbers are equal");
+}
