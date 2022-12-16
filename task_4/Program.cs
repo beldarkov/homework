9c6ced09055ab72845d17ff2556e7f82b7451773
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-16 10:05:17 +0300

    add task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a; 
+if(b > max) {max = b;}
+if(c > max) {max = c;}
+Console.WriteLine("max = " + max); 
