1b9953d586a953f1e04a33bd9480a079c0e7abc5
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-20 15:00:22 +0300

    made task 15

---
+++ task_15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine($"{day} -> нет");
+        break;
+    case 2:
+        Console.WriteLine($"{day} -> нет");
+        break;
+    case 3:
+        Console.WriteLine($"{day} -> нет");
+        break;
+    case 4:
+        Console.WriteLine($"{day} -> нет");
+        break;
+    case 5:
+        Console.WriteLine($"{day} -> нет");
+        break;
+    case 6:
+        Console.WriteLine($"{day} -> да");
+        break;
+    case 7:
+        Console.WriteLine($"{day} -> да");
+        break;
+    default:
+        Console.WriteLine("Такого дня нет!");
+        break; 
+}
