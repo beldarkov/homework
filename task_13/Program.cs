f4a8d2dbf1dfdf04fa6debe579afb652d1902962
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-20 14:49:11 +0300

    task 13

---
+++ task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
+else
+{
+
+    string third = Convert.ToString(num);
+    char ch = third[2];
+    Console.WriteLine($"{num} -> {ch}");
+}
