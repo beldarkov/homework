88fc5dfedaba1ce217040830adac25fc88571e07
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-23 15:22:08 +0300

    remake task 19

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.Write("Введите пятизначное число: ");
 string number = Console.ReadLine();
-int n = number.Length;
+int n = 5;
 int count = 0;
 
-if (n != 5) { Console.WriteLine("Ошибка! Введено не пятизначное число"); }
+if (number.Length != n) { Console.WriteLine("Ошибка! Введено не пятизначное число"); }
 else {
     for (int i = 0; i < number.Length / 2; i++)
     {
-        if (number[i] == number[n - i - 1]) { count++; }
+        if (number[i] == number[number.Length - i - 1]) { count++; }
     }
-    if (count == n / 2) { Console.WriteLine($"{number} -> да"); }
+    if (count == number.Length / 2) { Console.WriteLine($"{number} -> да"); }
     else { Console.WriteLine($"{number} -> нет"); }
 }
