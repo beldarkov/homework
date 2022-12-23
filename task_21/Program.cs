01d9494f85b034e496265a2c022d53ee5c0d3688
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2022-12-23 14:38:43 +0300

    task_21

---
+++ task_21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координату х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("Введите координату у1: ");
+int y1 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите координату х2: ");
+int x2 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите координату у2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+  
+double res = Math.Round(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)), 2);
+ 
+Console.WriteLine(res); 
