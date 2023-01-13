9018677f01507d8648a98d75f4e8c71a332b6fa0
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-13 10:20:36 +0300

    task 29

---
+++ task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[size]; 
+
+int[] FillArrFromKeybord(int[] arr) {
+    for(int i = 0; i < arr.Length; i++) {
+        Console.WriteLine($"Введите {i + 1}-ый элемент массива");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr; 
+}
+
+string result = string.Join(", ", FillArrFromKeybord(array)); 
+Console.WriteLine($"{result} -> [{result}]");  
