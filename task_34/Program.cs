694ec31dc5b35b9ebdb0a26591d5d5053c5c9797
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-17 08:45:50 +0300

    add task 34

---
+++ task_34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] FillArrayRandom(int[] arr) {
+    for(int i = 0; i < arr.Length; i++) {
+        arr[i] = new Random().Next(100, 1000); 
+    }
+    return arr; 
+}
+int CheckEven(int[] arr) {
+    int count = 0; 
+    for(int i = 0; i < arr.Length; i++) {
+        if(arr[i] % 2 == 0) {
+            count++; 
+        }
+    }
+    return count; 
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
+int[] mass = new int[size];
+FillArrayRandom(mass); 
+int result = CheckEven(mass); 
+System.Console.WriteLine((string.Join(" | ", mass)) + " -> " + result);
+
