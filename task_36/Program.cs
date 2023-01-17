6978a509a7b33945439eacfcf5ca52391ea2bd29
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-17 09:18:59 +0300

    task 36

---
+++ task_36/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArrayRandom(int[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+int SumOfOddIndex(int[] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        if (i % 2 != 0) {
+            sum += arr[i];
+        }
+    }
+    return sum; 
+}
+
+int size = 4;
+int[] array = new int[size];
+FillArrayRandom(array);
+int sum = SumOfOddIndex(array); 
+System.Console.WriteLine(string.Join(", ", array) + " -> " + sum);
