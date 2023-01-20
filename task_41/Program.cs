2e23cc1ff403d974e6e1af4ad82be427db09927b
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-20 11:07:53 +0300

    task 41

---
+++ task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] FillArrayFromKeyboard(int[] arr) {
+    for(int i = 0; i < arr.Length; i++) {
+        System.Console.Write($"Введите {i + 1}-ый элемент массива: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+    return arr; 
+}
+int CountNegative(int[] arr) {
+    int count = 0; 
+    for(int i = 0; i < arr.Length; i++) {
+        if(arr[i] < 0) {
+            count++;
+        }
+    }
+    return count; 
+}
+System.Console.Write("Введите размер массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M]; 
+FillArrayFromKeyboard(array);
+System.Console.WriteLine((string.Join(", ", array)) + " -> " + CountNegative(array));
