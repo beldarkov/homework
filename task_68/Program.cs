0ebc6332af69d79de95eb49e3653cb528d020683
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-31 15:09:32 +0300

    task 68

---
+++ task_68/Program.cs
@@ -0,0 +1,7 @@
+﻿int Akkerman(int a, int b) {
+    if(a == 0) return b + 1;
+    else if(a > 0 && b == 0) return Akkerman(a - 1, 1); 
+    else return Akkerman(a - 1, Akkerman(a, b - 1)); 
+    
+}
+System.Console.WriteLine(Akkerman(2, 3)); 
