df78205dccb5dd17a08ac4b745b4ffaa81ff2045
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-31 10:27:30 +0300

    task 64

---
+++ task_64/Program.cs
@@ -0,0 +1,9 @@
+﻿void PrintNumb(int a) {
+    if(a == 0) return; 
+    System.Console.Write(a + " ");
+    PrintNumb(a - 1);
+}
+
+System.Console.Write("Введите число - ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+PrintNumb(N); 
