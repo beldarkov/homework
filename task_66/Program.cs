7103ac8db2459bf9fca3593e74d48dbae1d9ec43
Alexander Mukhetdinov <hypcha4dream@gmail.com>
2023-01-31 10:45:57 +0300

    task 66

---
+++ task_66/Program.cs
@@ -0,0 +1,10 @@
+﻿int sum = 0;
+int Sum(int a, int b) {
+    if(a > b) return 1;
+    sum += b;
+    Sum(a, b - 1); 
+    return sum; 
+}
+int n = 5; 
+int m = 10; 
+System.Console.WriteLine("Сумма - " + Sum(n, m));
